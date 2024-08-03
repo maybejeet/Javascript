91d7d5ac471ad139ea6c1fc6b714a9023393db34
maybejeet <140199718+maybejeet@users.noreply.github.com>
2024-08-03 09:18:19 +0000

    learned about variables

---
+++ Basic_01/01_variables.js
@@ -0,0 +1,16 @@
+const id = 123456789
+var password = "password1"
+let emial = "jeet@gmail.com"
+city = "Siliguri"
+//let name;
+let names
+
+//console.log(id);
+
+password = "password123"
+email = "jkp@gmail.com"
+city = "jaipur"
+
+console.table([id,password,emial,city,names]);
+//console.log(name);
+
