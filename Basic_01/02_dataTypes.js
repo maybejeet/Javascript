fabb0ac97b93ca4bbe3d66212f559bf2b368fd6a
maybejeet <140199718+maybejeet@users.noreply.github.com>
2024-08-03 10:04:48 +0000

    datatypes

---
+++ Basic_01/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treal all in new version of js
+console.log(3+3);
+console.log("3+3");
+
+let age = 18
+let isLoggedIn = false 
+
+console.table([typeof "Jeet", typeof 231, typeof age, typeof isLoggedIn])
+
+//data types
+//number
+//bigint => for big numbers
+//string
+//boolean
+//null => standalone value
+//undefined
+//symbol
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+const foo = 100
+const foo1 = 100 +"1"
+const foo2 = 100+2
+
+console.table([foo,foo1,foo2])
+
