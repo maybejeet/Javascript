422d095ba79cd245a52548fffd5f3f033acc2d85
maybejeet <140199718+maybejeet@users.noreply.github.com>
2024-09-17 16:46:02 +0000

    conversion of datatype

---
+++ Basic_01/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score=33
+let score1="33abc"
+let score2=undefined
+// console.log(typeof score2)
+
+let valueInNumber2 = Number(score2)
+// console.log(valueInNumber2)
+// console.log(typeof valueInNumber2)
+
+// console.log(typeof score)
+// console.log(score)
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+let valueInNumber1 = Number(score1)
+// console.log(valueInNumber1)
+// console.log(typeof valueInNumber1)
+
+//number ko convert krne se convert ho jata h
+//"33abc" convert hoke NaN ban jata h
+//true=1 false=0
+
+let isLoggedIn="0"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+//1 -> true
+//0 -> false
+//"" -> false
+//"hwjdfb" -> true
+
+let someVariable = "69sddc"
+let stringVariable = String(someVariable)
+console.log(stringVariable)
+console.log(typeof stringVariable)
