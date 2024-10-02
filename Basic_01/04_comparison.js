5e13262054acd5e657b9f86037d9cae5c6dadd8d
maybejeet <140199718+maybejeet@users.noreply.github.com>
2024-10-02 09:18:21 +0000

    comparisons

---
+++ Basic_01/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2==3);
+// console.log(2!=3);
+
+
+// console.log(2<"3"); //string ko number ke sath. string number me convert ho gaya
+// console.log(2==3);
+
+//=== strict ckechk, also checks the datatype. 
+// console.log("2"===3); //isme string to number conversion nhi hua
+// console.log(3===3);
+
+
+
+//TRY AVOIDING THESE CONVERSIONS
+// console.log(null > 0);//comparison operator and equality check works differently in js. Comparison me null ka value 0 ho jata h
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0); 
+
+// console.log(undefined > 0);//undefined ke case me sab false
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0); 
