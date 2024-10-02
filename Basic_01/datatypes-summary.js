1952d67ccf7caa58c9e6968034cdd05f007139a6
maybejeet <140199718+maybejeet@users.noreply.github.com>
2024-10-02 09:48:12 +0000

    datatype-summary

---
+++ Basic_01/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+// 7types: String, Number, Boolean, null, undefined, Symbol(Kisi bhi value ko unique banane ke liye), BigInt
+
+// const Id = Symbol('123')
+// const Id2 = Symbol('123')
+// console.log(Id == Id2); //Symbol() me same value dalne se bhi Id and Id2 ka value same nhi
+// console.log(typeof Id); //type:symbol
+
+
+// Reference (Non primitive)
+// Arrays, Objects, Functions
+
+// const friends=["Neha","Shreya","Saloni","Ayush","Anand","Aishika"]
+// console.log(friends);
+// console.log(typeof friends);
+
+
+// let myObject = {
+//     name : "Jeet",
+//     roll : 11900123063,
+//     age : 20,
+// }
+// console.log(myObjec);
+// console.log(typeof myObject);
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+myFunction()
+console.log(typeof myFunction);
+
+
+
+
