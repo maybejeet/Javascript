282e0231621b14ffa94bbf9e5cf647ca95d3e172
maybejeet <140199718+maybejeet@users.noreply.github.com>
2024-10-09 14:55:50 +0000

    math and num

---
+++ Basic_01/06_nums&math.js
@@ -0,0 +1,29 @@
+const score = 400
+const balance = new Number (1000)
+
+//console.log(score)
+//console.log(balance)
+
+//console.log(balance.toString())
+//console.log(balance.toFixed(4))
+
+const otherNumber = 122343.621
+//console.log(otherNumber.toPrecision(1))
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//console.log(Math.round(4.4))
+//console.log(Math.round(4.6))
+//console.log(Math.ceil(4.4))
+//console.log(Math.floor(4.6))
+//console.log(Math.min(2,34,234,234,23,12))
+//console.log(Math.max(2,34,234,234,23,12))
+
+//console.log(Math.random())
+//console.log(Math.floor((Math.random()*10)+1))
+
+const min = 100
+const max = 200
+
+console.log(Math.floor(((Math.random()*(max-min+1))+min))) //Random number bw max and min formula
